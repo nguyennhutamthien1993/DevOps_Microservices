3e9c42a282f6f9cd32a40341d0669fe83a66656a
Thiên <thiennnt@stisolutions.net>
2022-08-10 20:56:44 +0700

    Adding containerized flow

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=thiennnt/devops .
+docker build --tag=thiennnt/devops
 
 # Step 2: 
 # List docker images
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=thiennnt/devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run devops --image=thiennnt/devops --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devops 8000:80
